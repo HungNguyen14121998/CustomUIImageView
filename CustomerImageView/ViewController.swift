2cdb5f70787fb8029041506fc7d0b3f3dc11d0c0
NguyenHuuHung <hhnguyenit@gmail.com>
2021-06-18 14:47:03 +0700

    Initial Commit

---
+++ CustomerImageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomerImageView
+//
+//  Created by Nguyen Huu Hung on 6/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
